238639aba7d6af632357116b98198734a979dcc6
ScMohanty <92923028+ScMohanty@users.noreply.github.com>
2021-10-29 10:03:21 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParticipentDetails1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string s = ",";
+            StringBuilder sb = new StringBuilder();
+
+            Console.WriteLine("ENTER THE NAME");
+            sb.Append(Console.ReadLine());
+            sb.Append(",");
+
+            Console.WriteLine("ENTER THE EMPnO");
+            sb.Append(Console.ReadLine());
+            sb.Append(",");
+
+            Console.WriteLine("ENTER THE DESIGNATION");
+            sb.Append(Console.ReadLine());
+            sb.Append(",");
+           
+            Console.WriteLine("ENTER THE EXPERIENCE");
+            sb.Append(Console.ReadLine());
+            sb.Append(",");
+
+            Console.WriteLine("ENTER THE QUALIFICATION");
+            sb.Append(Console.ReadLine());
+
+            Console.WriteLine(sb);
+
+        }
+    }
+}
